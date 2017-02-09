38ee0208e1223051eb5095389cce1595379208fc
begimov <begimov@gmail.com>
2017-02-09 14:58:24 +0300

    app configs for development and production mode added

--- app/start.php
+++ app/start.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\App;
+use Noodlehaus\Config;
 
 session_cache_limiter(false);
 session_start();
@@ -8,7 +9,11 @@ session_start();
 ini_set('display_errors', 'On');
 
 define('INC_ROOT', dirname(__DIR__));
+define('MODE', 'development');
 
 require INC_ROOT . '/vendor/autoload.php';
+$settings = require INC_ROOT . '/app/config/' . MODE . '.php';
 
-$app = new App();
+$app = new App($settings);
+
+print_r($app->getContainer()->get('app')['db']['driver']);
